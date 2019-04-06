6ef5c590eb73f2a54e21ca20449d2ab63b056c18
RaquelCC <rcanalesconcha@gmail.com>
2019-04-06 00:48:34 -0300

    Updates

---
+++ precache-manifest.afc285d0a28f67fe65564b59812fcf59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9f7c061fd81e44c44e7",
+    "url": "/FavStores/static/css/main.d870741e.chunk.css"
+  },
+  {
+    "revision": "f9f7c061fd81e44c44e7",
+    "url": "/FavStores/static/js/main.f9f7c061.chunk.js"
+  },
+  {
+    "revision": "d37165e2a025686151e8",
+    "url": "/FavStores/static/js/runtime~main.d37165e2.js"
+  },
+  {
+    "revision": "8051f8a14173cf33faed",
+    "url": "/FavStores/static/js/2.8051f8a1.chunk.js"
+  },
+  {
+    "revision": "86f62ba9ba225817a557a35522eb719c",
+    "url": "/FavStores/index.html"
+  }
+];
